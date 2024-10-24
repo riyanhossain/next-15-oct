b4161e247981a01df158d9b4957912372c3c879b
riyanhossain <mehraabriyan@gmail.com>
2024-10-24 21:15:17 +0600

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
+import { db } from "@/db";
+import { usersTable } from "@/db/schema";
 import Image from "next/image";
 
 export default function Home() {
+  // fetch data from the database
+  async function fetchData() {
+    const users = await db.select().from(usersTable);
+    console.log(users);
+  }
+
+  fetchData();
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
---
+++ src/db/index.ts
@@ -0,0 +1,46 @@
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/libsql";
+import { createClient } from "@libsql/client";
+import { eq } from "drizzle-orm";
+import { usersTable } from "@/db/schema";
+
+export const client = createClient({
+  url: process.env.DB_FILE_NAME!,
+});
+
+export const db = drizzle(client);
+
+async function main() {
+  const user: typeof usersTable.$inferInsert = {
+    name: "John",
+    age: 30,
+    email: "john@example.com",
+  };
+
+  await db.insert(usersTable).values(user);
+  console.log("New user created!");
+
+  const users = await db.select().from(usersTable);
+  console.log("Getting all users from the database: ", users);
+  /*
+  const users: {
+    id: number;
+    name: string;
+    age: number;
+    email: string;
+  }[]
+  */
+
+  await db
+    .update(usersTable)
+    .set({
+      age: 31,
+    })
+    .where(eq(usersTable.email, user.email));
+  console.log("User info updated!");
+
+  // await db.delete(usersTable).where(eq(usersTable.email, user.email));
+  // console.log("User deleted!");
+}
+
+main();
